3eeb3bb649f71e1897877ba448578e05cd0a290a
Josh Varty <joshvarty@gmail.com>
2017-06-02 10:52:55 -0700

    Move UnnecessaryCode to ClassificationTypeNames

--- src/EditorFeatures/Core/Implementation/Diagnostics/ClassificationTypeDefinitions.cs
+++ src/EditorFeatures/Core/Implementation/Diagnostics/ClassificationTypeDefinitions.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System.ComponentModel.Composition;
+using Microsoft.CodeAnalysis.Classification;
 using Microsoft.VisualStudio.Language.StandardClassification;
 using Microsoft.VisualStudio.Text.Classification;
 using Microsoft.VisualStudio.Utilities;
@@ -9,16 +10,14 @@ namespace Microsoft.CodeAnalysis.Editor.Implementation.Diagnostics
 {
     internal sealed class ClassificationTypeDefinitions
     {
-        public const string UnnecessaryCode = "unnecessary code";
-
         [Export]
-        [Name(UnnecessaryCode)]
+        [Name(ClassificationTypeNames.UnnecessaryCode)]
         [BaseDefinition(PredefinedClassificationTypeNames.FormalLanguage)]
         internal ClassificationTypeDefinition UnnecessaryCodeTypeDefinition { get; set; }
 
         [Export(typeof(EditorFormatDefinition))]
-        [ClassificationType(ClassificationTypeNames = UnnecessaryCode)]
-        [Name(UnnecessaryCode)]
+        [ClassificationType(ClassificationTypeNames = ClassificationTypeNames.UnnecessaryCode)]
+        [Name(ClassificationTypeNames.UnnecessaryCode)]
         [Order(After = Priority.High)]
         [UserVisible(false)]
         private class UnnecessaryCodeFormatDefinition : ClassificationFormatDefinition
